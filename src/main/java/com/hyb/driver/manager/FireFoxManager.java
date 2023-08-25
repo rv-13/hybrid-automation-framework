c70ef7e5ca4908ec4e17218706c56b688a88f448
rv-13 <r131194@gmail.com>
2023-08-25 23:09:57 +0530

    driver manager classes

---
+++ src/main/java/com/hyb/driver/manager/FireFoxManager.java
@@ -0,0 +1,18 @@
+package com.hyb.driver.manager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public final class FireFoxManager {
+
+    private FireFoxManager() {
+    }
+
+    public static WebDriver getDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
+
+
+}
